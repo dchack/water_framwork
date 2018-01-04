6a921452df7c3cc5877e1acef5ca09e759c117a7
bob <bob@xiaokakeji.com>
2018-01-04 11:29:07 +0800

    quartz test

--- hope-learn-third/src/java/com/hope/learn/third/quartz/test/MyJob.java
+++ hope-learn-third/src/java/com/hope/learn/third/quartz/test/MyJob.java
@@ -2,6 +2,9 @@ package com.hope.learn.third.quartz.test;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.SchedulerException;
+
+import java.util.List;
 
 /**
  * Created by dongchao on 2018/1/3.
@@ -11,6 +14,19 @@ public class MyJob implements org.quartz.Job {
     }
 
     public void execute(JobExecutionContext context) throws JobExecutionException {
+
+        try {
+            List<JobExecutionContext> list = context.getScheduler().getCurrentlyExecutingJobs();
+            for(JobExecutionContext jobExecutionContext : list){
+                // job内部获取容器内变量
+                System.out.printf(jobExecutionContext.getJobDetail().getKey().getGroup());
+            }
+            Thread.sleep(5000);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.err.println("Hello World!  MyJob is executing.");
     }
 }
--- hope-learn-third/src/java/com/hope/learn/third/quartz/test/QuartzTest.java
+++ hope-learn-third/src/java/com/hope/learn/third/quartz/test/QuartzTest.java
@@ -22,6 +22,10 @@ public class QuartzTest {
                     .withIdentity("job1", "group1")
                     .build();
 
+            JobDetail job2 = JobBuilder.newJob(MyJobT.class)
+                    .withIdentity("job2", "group2")
+                    .build();
+
             // Trigger the job to run now, and then repeat every 40 seconds
             Trigger trigger = TriggerBuilder.newTrigger()
                     .withIdentity("trigger1", "group1")
@@ -30,6 +34,15 @@ public class QuartzTest {
                             .withIntervalInSeconds(4)
                             .repeatForever())
                     .build();
+
+            Trigger trigger2 = TriggerBuilder.newTrigger()
+                    .withIdentity("trigger2", "group2")
+                    .startNow()
+                    .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                            .withIntervalInSeconds(2)
+                            .repeatForever())
+                    .build();
+
 
             // Tell quartz to schedule the job using our trigger
             scheduler.scheduleJob(job, trigger);
