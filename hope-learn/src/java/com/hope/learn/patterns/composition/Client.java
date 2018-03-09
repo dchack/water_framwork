4c0965726b910c8f5f3029e3d0a79643b03bd5ab
bob <bob@xiaokakeji.com>
2018-03-09 21:50:30 +0800

    composite

--- hope-learn/src/java/com/hope/learn/patterns/composition/Client.java
+++ hope-learn/src/java/com/hope/learn/patterns/composition/Client.java
@@ -6,6 +6,12 @@ package com.hope.learn.patterns.composition;
 public class Client {
 
     public static void main(String[] args) {
+        Component composite = new Composite("Composite");
+        Component leaf1 = new Leaf("leaf1");
+        Component leaf2 = new Leaf("leaf2");
+        composite.add(leaf1);
+        composite.add(leaf2);
 
+        composite.display();
     }
 }
